ed36575b5527cdb2fbca72c68fba4a6f9ade5739
Artur Mustafin <funcelot@outlook.com>
2022-01-27 19:29:42 +0000

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-a = (4, 5, 6)
-
-LST = lambda *x: x
-
 counter = 0
-
 
 def debugger(name, debug=False):
     def wrapper(f):
@@ -16,23 +11,26 @@ def debugger(name, debug=False):
         return func
     return wrapper
 
-
+LST = lambda *x: x
 CAR = lambda x: x[0] if x else []
 CDR = lambda x: x[1:]
 LEN = lambda x: 1 + LEN(CDR(x)) if x else 0
-# SUM = lambda x: CAR(x) + SUM(CDR(x)) if CDR(x) else CAR(x)
 ACC = lambda x, f: f(LST(CAR(x), ACC(CDR(x), f))) if CDR(x) else CAR(x)
 SUM = lambda x: ACC(x, sum)
 FLT = lambda *x: LST(*FLT(CAR(x)), *FLT(*CDR(x))) if CDR(x) else CAR(x)
 
-
+LST = debugger("LST", debug=True)(LST)
 CAR = debugger("CAR", debug=True)(CAR)
-CAR = debugger("CDR", debug=True)(CAR)
+CDR = debugger("CDR", debug=True)(CDR)
 LEN = debugger("LEN", debug=True)(LEN)
 ACC = debugger("ACC", debug=True)(ACC)
 SUM = debugger("SUM", debug=True)(SUM)
 FLT = debugger("FLT", debug=True)(FLT)
 
+a = (1, 2, 3)
+b = LST(4, 5, 6)
+c = (7, 8)
+d = (9,)
 
 print(":", a)
 print(":", CAR(a))
@@ -40,10 +38,6 @@ print(":", CDR(a))
 print(":", LEN(a))
 print(":", SUM(a))
 print(":", LST(1, 2, 3))
-
-b = LST(1, 2, 3)
-c = (7, 8)
-d = ((9,),)
 
 print(":", CAR(b))
 print(":", CAR(LST(CAR(b))))
